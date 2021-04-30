d15e8500785eb6058806053cfb20d35b581ae018
ElenaPrepodobnaja <73935522+ElenaPrepodobnaja@users.noreply.github.com>
2021-04-30 11:10:04 +0300

    Update gallery-list.js

--- src/gallery-list.js
+++ src/gallery-list.js
@@ -2,12 +2,11 @@ import images from './gallery-items.js';
 
 const list = document.querySelector('.js-gallery');
 const galleryList = createListImages(images);
-
+list.insertAdjacentHTML('beforeend', galleryList);
 function createListImages(images) {
     return images
-        .map(({ preview, original, description }) => {
-            return
-            `<li class="gallery__item">
+      .map(({ preview, original, description }) => {
+          return `<li class="gallery__item">
       <a
     class="gallery__link"
     href="${original}"
@@ -21,8 +20,61 @@ function createListImages(images) {
       </a>
       </li>    
            `;
-        }).join('');
+        }).join(''); 
     
 };
 
-list.insertAdjacentHTML('beforeend', galleryList);
+list.addEventListener('click', onImageLinkClick);
+const modalWindow = document.querySelector('.lightbox');
+const modalImageEl = document.querySelector('.lightbox__image');
+
+function onImageLinkClick(event) {
+  
+  event.preventDefault();
+ 
+  const imgFullUrl = event.target.dataset.source;
+  const altTextFromImg = event.target.alt;
+
+
+  modalImageEl.src = imgFullUrl;
+  modalImageEl.alt = altTextFromImg;
+  modalWindow.classList.add('is-open');
+  modalImageEl.classList.add('appear');
+
+  window.addEventListener('keydown', arrowImagesSwitch);
+}
+
+
+const closeModalBtn = document.querySelector('.lightbox__button');
+
+closeModalBtn.addEventListener('click', onCloseBtnClick);
+
+window.addEventListener('keydown', onCloseBtnClick);
+
+const backdropEl = document.querySelector('.lightbox__overlay');
+backdropEl.addEventListener('click', onCloseBtnClick);
+function onCloseBtnClick(event) {
+  if (event.code === 'Escape'|| event.currentTarget === event.target) {
+    
+    modalWindow.classList.remove('is-open');
+    modalImageEl.src = '';
+  }
+
+}
+
+
+function arrowImagesSwitch(e) {
+  const imagesSrcArray = images.map(image => image.original);
+  const indexOfCurrentImg = imagesSrcArray.indexOf(modalImageEl.src);
+
+  if (e.code === 'ArrowRight') {
+    if (indexOfCurrentImg < imagesSrcArray.length - 1)
+      modalImageEl.src = imagesSrcArray[Number(indexOfCurrentImg) + 1];
+  }
+
+  if (e.code === 'ArrowLeft') {
+    if (indexOfCurrentImg > 0) {
+      modalImageEl.src = imagesSrcArray[Number(indexOfCurrentImg) - 1];
+    }
+  }
+}
